4eef17e126047dc28b785e3c2a35ec6c36f84c70
Amine Ouazad <aouazad@gmail.com>
2024-08-27 23:32:38 -0400

    now downloads the Pavlov independent archive in the folder directly

--- 03_flaws_in_the_independent_replication_table_8/download_independent_replication.sh
+++ 03_flaws_in_the_independent_replication_table_8/download_independent_replication.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-wget https://www.dropbox.com/scl/fi/bsy3vho7dvvh23lwdg5zz/lacour_little_data_archive_initial_submission.zip?rlkey=tznr0xbeuelipjvalvio2hir0&st=idejl55x&dl=1 -P downloads/
 
 cd downloads/
 
-mv lacour_little_data_archive_initial_submission.zip?rlkey=tznr0xbeuelipjvalvio2hir0&st=idejl55x&dl=1 lacour_little_data_archive_initial_submission.zip
+wget https://www.dropbox.com/scl/fi/bsy3vho7dvvh23lwdg5zz/lacour_little_data_archive_initial_submission.zip?rlkey=tznr0xbeuelipjvalvio2hir0&st=idejl55x
+
 unzip lacour_little_data_archive_initial_submission.zip
 
 cd ..
