584396acbb977bb7fc5a6d642ff84b6e8e75f0b5
Amine Ouazad <aouazad@gmail.com>
2024-10-03 20:14:47 -0400

    Script to run all regressions at once (takes the night)

---
+++ 01_reproduction_of_ouazad_and_kahn/run_everything.R
@@ -0,0 +1,13 @@
+
+source("06_main_regression.R")
+source("07_specification_tests_OLS.R")
+source("08_specification_tests_2wfe.R")
+source("09_specification_tests_agency.R")
+source("10_specification_tests_agency_high_cost.R")
+source("11_specification_tests_high_cost.R")
+source("12_specification_tests_high_cost_year_interactions.R")
+source("13_specification_tests_agency_high_cost_year_interactions.R")
+source("14_main_regression_belowlimit_highcost_year_interactions.R")
+source("15_specification_tests_rounding")
+source("20_make_summary_tables.R")
+
